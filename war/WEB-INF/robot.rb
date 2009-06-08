5853c438b07a1a743614b7e0d3c0a0ab0a73dbc1
unknown <Michael@.(none)>
2009-06-07 18:02:00 -0700

    Don't use robot.yml anymore

--- war/WEB-INF/robot.rb
+++ war/WEB-INF/robot.rb
@@ -1,23 +1,17 @@
 require 'lib/waveapi/init'
 
 class Robot < AbstractRobot
-  def extra_commands
-    ["name", "whine"] #Again, whine is here because Wave isn't reloading capabilities
-  end
-  def name(params)
-    @name
-  end
+  set_name "Sinatra Bot"
+  add_cron :clock, 20
+  
   def DOCUMENT_CHANGED(properties, context)
-    wavelet = context.GetWavelets()[0]
+    wavelet = context.GetWavelets[0]
     blip = context.GetBlipById(wavelet.GetRootBlipId())
-    blip.GetDocument().SetText('Only I get to edit the top blip!')
-  end
-  def whine(event, context)
-    clock(event, context) #Wave isn't reading in the new capabilities info.
+    blip.GetDocument.SetText('Only I get to edit the top blip!')
   end
   def clock(event, context)
-    wavelet = context.GetWavelets()[0]
+    wavelet = context.GetWavelets[0]
     blip = context.GetBlipById(wavelet.GetRootBlipId())
-	blip.GetDocument().SetText("It's " + Time.now.to_s)
+	blip.GetDocument.SetText("It's " + Time.now.to_s)
   end
 end
