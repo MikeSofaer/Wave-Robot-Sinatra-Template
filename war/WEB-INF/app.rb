b668c9e06c17c8b43682482a6228e3f9a9a753ce
unknown <Michael@.(none)>
2009-06-07 18:19:31 -0700

    Don't compare symbols to strings

--- war/WEB-INF/app.rb
+++ war/WEB-INF/app.rb
@@ -27,7 +27,7 @@ post '/_wave/robot/:command' do
   logger.info "Input: " + request.env['rack.request.form_vars']
   json = JSON(request.env['rack.request.form_vars'])
   content_type 'application/json' 
-  context = robot.run_command(params[:command], json)
+  context = robot.run_command(params[:command].to_sym, json)
   output = AbstractRobot.serialize_context(context)
   logger.info "Output: " + output
   output
